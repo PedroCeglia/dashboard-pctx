b21f323442f79955d559c279033e5c5721f31edc
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-10-01 13:54:30 -0300

    import and use ItemTableRow, create   itemsSelectedList, setItemsSelectedList, props

--- src/components/EstruturaTable/TableBody/index.jsx
+++ src/components/EstruturaTable/TableBody/index.jsx
@@ -1,17 +1,31 @@
-import { TableBodyStyle } from "../style";
+import { TableBodyStyle } from "./style";
+import ItemTableRow from "./ItemTableRow";
 
-export default function TableBody({filterTableList, headerTablePropsList}){
-    return(
-        <TableBodyStyle>
-            {filterTableList.map((item, id) => {
-            return (
-                <tr key={"tbody tableList " + id}>
-                {headerTablePropsList.map((props, id) => {
-                    return <td key={"td tableHeaderList " + id}>{item[props]}</td>;
-                })}
-                </tr>
-            );
+export default function TableBody({
+  filterTableList,
+  tableTextPropsList,
+  itemsSelectedList,
+  setItemsSelectedList,
+}) {
+  return (
+    <TableBodyStyle>
+      {filterTableList.map((item, id) => {
+        return (
+          <ItemTableRow
+            key={"tbody tableList " + id}
+            itemRow={item}
+            itemsSelectedList={itemsSelectedList}
+            setItemsSelectedList={setItemsSelectedList}
+          >
+            {tableTextPropsList.map((props, id) => {
+              if (props !== "isChecked")
+                return (
+                  <td key={"td tableHeaderList " + id}> {item[props]} </td>
+                );
             })}
-        </TableBodyStyle>
-    )
+          </ItemTableRow>
+        );
+      })}
+    </TableBodyStyle>
+  );
 }
