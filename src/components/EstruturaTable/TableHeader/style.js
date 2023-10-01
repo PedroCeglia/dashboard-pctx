da02f5afeda400dcf6e20f1728da7c4a501e9136
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-10-01 13:56:39 -0300

    Create TableHeader Style

---
+++ src/components/EstruturaTable/TableHeader/style.js
@@ -0,0 +1,27 @@
+import styled from "styled-components"
+
+
+export const TableHeaderStyle = styled.thead`
+  width: 100%;
+  position: sticky;
+  top: 100px;
+  z-index: 1;
+
+  background-color: white;
+  border-top-right-radius: 10px;
+  border-top-left-radius: 10px;
+  
+  tr{ 
+    width: 100%;
+    background-color: rgb(159 159 159);
+    padding: 20px;
+    border-bottom: 1px solid black;
+  }
+  tr:nth-of-type(1) th{
+    padding: 20px;
+    column-width: 20vw;
+  }
+  tr:nth-of-type(1) th:nth-of-type(1){
+    width:30px;
+  }
+`;
