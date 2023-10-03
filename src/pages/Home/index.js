428cb20dfda13e7813929f54ceba48f83b056557
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-10-03 18:58:49 -0300

    clean the code

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,22 +4,6 @@ import { useState, useEffect } from "react";
 
 export default function Home() {
   const [estoqueList, setEstoqueList] = useState([{}]);
-  /*useEffect(()=>{
-    console.log(estoqueList)
-  },[estoqueList])
-  
-  useEffect(() => {
-    // Chame a função getEstoque e, em seguida, defina a lista de estoque
-    getEstoque()
-      .then((res) => {
-        //console.log(res)
-        setEstoqueList(res); // Defina a lista de estoque com os dados recebidos
-      })
-      .catch((error) => {
-        console.error("Erro ao buscar estoque:", error);
-      });
-  }, []); // Esta função useEffect será executada apenas uma vez, quando o componente for montado
-*/
   useEffect(()=>{
     getEstoque(setEstoqueList)
   },[])
