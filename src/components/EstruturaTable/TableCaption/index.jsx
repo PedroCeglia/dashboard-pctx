1e0e3daf91d31c234e362eef27d26838bcd506ee
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-10-05 16:54:20 -0300

    Change the "button" that add a new item to a "Link"

--- src/components/EstruturaTable/TableCaption/index.jsx
+++ src/components/EstruturaTable/TableCaption/index.jsx
@@ -1,4 +1,5 @@
 import { TableCaptionStyle } from "./style";
+import { Link } from "react-router-dom";
 
 export default function TableCaption({itemsSelectedList = [], setItemsSelectedList}) {
 
@@ -10,7 +11,7 @@ export default function TableCaption({itemsSelectedList = [], setItemsSelectedLi
         <h2>Estoque </h2>
         <div>
           {itemsSelectedList.length > 1 && <button onClick={disselectAllItens}>Disselect All</button>}
-          <button>Add +</button>
+          <Link to="/estoque/add">Add +</Link>
           <button>Delete</button>
         </div>
       </div>
--- src/components/EstruturaTable/TableCaption/style.js
+++ src/components/EstruturaTable/TableCaption/style.js
@@ -10,26 +10,35 @@ export const TableCaptionStyle = styled.caption`
   background-color: white;
 
   & > div {
-    height: 100px;
+    height: 70px;
+    padding-top: 10px;
     width: 100%;
     padding: 20px;
     display: flex;
     justify-content: space-between;
     align-items: center;
 
-    background-color:rgba(0, 0, 0, 0.3);
+    background-color: rgba(0, 0, 0, 0.3);
     padding: 10px 20px;
   }
   div h2 {
     margin: 0 20px;
   }
 
+  div a,
   div button {
-    padding: 5px 10px;
-    margin: 5px 20px;
+    padding: 10px 20px;
+    margin: 0 20px;
+
     cursor: pointer;
+    text-decoration: none;
+
     background-color: rgba(0, 0, 200, 0.1);
     border: 1px solid rgba(0, 0, 0, 0.1);
     border-radius: 10px;
+
+    color: black;
+    font-size: 1em;
+    font-weight: bold;
   }
 `;
