d34d12d6387a89acfacf6e9ba3e2c9ca94f7f214
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-10-05 12:12:48 -0300

    Create AuthSingIn Component

---
+++ src/components/AuthSingIn/index.jsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import { AuthSingInStyle, InputContainerStyle } from "./style";
+
+export default function AuthSingIn({ isCreating = true }) {
+  const [nameField, setNameField] = useState("");
+  const [emailField, setEmailField] = useState("");
+  const [passwordField, setPasswordField] = useState("");
+  const [confirmPasswordField, setConfirmPasswordField] = useState("");
+
+  function createAccount() {
+    if (verifyNameField())
+      if (verifyEmailField())
+        if (verifyPasswordField())
+          if (verifyPasswordConfirmField()) console.log("OIOI 1");
+  }
+  function loginAccount() {
+    if (verifyEmailField()) if (verifyPasswordField()) console.log("OIOI 1");
+  }
+
+  function verifyNameField() {
+    if (nameField.length < 4) return;
+    return true;
+  }
+  function verifyEmailField() {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValid = emailRegex.test(emailField);
+    if (!isValid) return;
+    return true;
+  }
+  function verifyPasswordField() {
+    if (passwordField.length < 6) return;
+    return true;
+  }
+  function verifyPasswordConfirmField() {
+    if (passwordField !== confirmPasswordField) return;
+    return true;
+  }
+
+  return (
+    <AuthSingInStyle>
+      <form>
+        {isCreating && (
+          <InputContainerStyle>
+            <label>Digite o seu Nome</label>
+            <input
+              type="text"
+              placeholder="Seu Nome Aqui"
+              onChange={(e) => setNameField(e.target.value)}
+              value={nameField}
+            />
+          </InputContainerStyle>
+        )}
+        <InputContainerStyle>
+          <label>Digite o seu Email</label>
+          <input
+            type="email"
+            placeholder="seuemail@aqui.com"
+            onChange={(e) => setEmailField(e.target.value)}
+            value={emailField}
+          />
+        </InputContainerStyle>
+        <InputContainerStyle>
+          <label>Crie sua senha</label>
+          <input
+            type="password"
+            placeholder="***********"
+            onChange={(e) => setPasswordField(e.target.value)}
+            value={passwordField}
+          />
+        </InputContainerStyle>
+
+        {isCreating && (
+          <InputContainerStyle>
+            <label>Confirme sua senha</label>
+            <input
+              type="password"
+              placeholder="***********"
+              onChange={(e) => setConfirmPasswordField(e.target.value)}
+              value={confirmPasswordField}
+            />
+          </InputContainerStyle>
+        )}
+      </form>
+
+      <button onClick={isCreating ? createAccount : loginAccount}>
+        Criar Conta
+      </button>
+      <button>
+        {isCreating ? "Você Ja possui uma conta" : "Não Pussue uma conta?"}
+      </button>
+    </AuthSingInStyle>
+  );
+}
