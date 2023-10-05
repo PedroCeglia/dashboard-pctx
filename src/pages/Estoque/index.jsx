976d57492f2d7bfd96d3817fc59436ddfc6e7935
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-10-05 16:58:32 -0300

    Get Estoque by database

--- src/pages/Estoque/index.jsx
+++ src/pages/Estoque/index.jsx
@@ -1,31 +1,20 @@
-import "@syncfusion/ej2-base/styles/material.css";
-import "@syncfusion/ej2-buttons/styles/material.css";
-import "@syncfusion/ej2-calendars/styles/material.css";
-import "@syncfusion/ej2-dropdowns/styles/material.css";
-import "@syncfusion/ej2-inputs/styles/material.css";
-import "@syncfusion/ej2-navigations/styles/material.css";
-import "@syncfusion/ej2-popups/styles/material.css";
-import "@syncfusion/ej2-splitbuttons/styles/material.css";
-import "@syncfusion/ej2-react-grids/styles/material.css";
-
-import {
-  ColumnDirective,
-  ColumnsDirective,
-  GridComponent,
-  Filter, Group, Inject, Page, Sort 
-} from "@syncfusion/ej2-react-grids";
-
+import EstruturaTable from "../../components/EstruturaTable";
+import { getEstoque } from "../../api/database";
+import { useState, useEffect } from "react";
+import { useLocation, Outlet } from "react-router-dom";
 
 export default function Estoque() {
+
+  const [estoqueList, setEstoqueList] = useState([{}]);
+  useEffect(() => { getEstoque(setEstoqueList) }, []);
+
+  const pathname = useLocation().pathname
+  const isAdding = pathname === "/estoque/add"
+  
   return (
-    <GridComponent dataSource={[]} width={1000}  pageSettings={true}>
-      <ColumnsDirective>
-        <ColumnDirective field="NotaFiscal" width="100" textAlign="Right" />
-        <ColumnDirective field="id" width="100" textAlign="Right" />
-        <ColumnDirective field="NomeDoProduto" width="100" textAlign="Right" />
-        <ColumnDirective field="ValorDoProduto" width="100" textAlign="Right" />
-      </ColumnsDirective>
-      <Inject services={[Page, Sort, Filter, Group]}/>
-    </GridComponent>
+    <>
+      { isAdding && <Outlet/> }
+      <EstruturaTable tableList={estoqueList} />
+    </>
   );
 }
