fcf9c1d19963594c18d5e5f733b5278a85569ac5
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-10-05 16:58:14 -0300

    Verify the type of  auth by the pathname

--- src/pages/AuthPage/index.jsx
+++ src/pages/AuthPage/index.jsx
@@ -1,5 +1,9 @@
 import AuthSingIn from "../../components/AuthSingIn";
+import { useLocation } from "react-router-dom";
 
 export default function AuthPage() {
-  return <AuthSingIn />;
+  const pathname = useLocation().pathname;
+  const isCreating = pathname === "/auth/sing-in";
+
+  return <AuthSingIn isCreating={isCreating} />;
 }
