3139a082997da6fe5266c89022e7703dde6dfe2d
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-10-18 15:29:15 -0300

    Create ValidateResponseStyle

--- src/pages/AddEstoque/ValidateResponse/index.jsx
+++ src/pages/AddEstoque/ValidateResponse/index.jsx
@@ -1,13 +1,14 @@
 import TotalAmount from "./TotalAmount";
+import { ValidateResponseStyle } from "./style";
 
 export default function ValidateResponse(){
     return(
-        <div>
+        <ValidateResponseStyle>
             <h2>Informações da Nota Fiscal</h2>
             <TotalAmount/>
             <>Produtos</>
             <>total</>
             <button>Validar Nota</button>
-        </div>
+        </ValidateResponseStyle>
     )
 }
---
+++ src/pages/AddEstoque/ValidateResponse/style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+export const ValidateResponseStyle = styled.div`
+
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    h2{
+        margin: 20px;
+    }
+
+`
