91bf526b890302f715dcce63394ae1f7b379a494
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-10-19 08:03:18 -0300

    create a style to "button"

--- src/pages/AddEstoque/ValidateResponse/style.js
+++ src/pages/AddEstoque/ValidateResponse/style.js
@@ -7,9 +7,17 @@ export const ValidateResponseStyle = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    background-color: rgb(163 167 205);
+    
 
     h2{
         margin: 20px;
     }
 
+    button{
+        padding: 10px 20px ;
+        background-color: rgba(255,255,255,.7);
+        border: 1px solid rgba(0,0,0,.8);
+        margin: 0 0 40px ;
+    }
 `
