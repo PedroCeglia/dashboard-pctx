19d2e766d74410a393353eb8f6e908deb5bd6cf8
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-10-19 08:02:18 -0300

    Import and use "ProductsToAdd" & "TotalAmount"

--- src/pages/AddEstoque/ValidateResponse/index.jsx
+++ src/pages/AddEstoque/ValidateResponse/index.jsx
@@ -1,13 +1,17 @@
-import EmitenteRemetente from "./EmitenteRemetente";
 import { ValidateResponseStyle } from "./style";
+
+import EmitenteRemetente from "./EmitenteRemetente";
+import ProductsToAdd from "./ProductsToAdd";
+import TotalAmount from "./TotalAmount";
+
 
 export default function ValidateResponse(){
     return(
         <ValidateResponseStyle>
             <h2>Informações da Nota Fiscal</h2>
             <EmitenteRemetente/>
-            <>Produtos</>
-            <>total</>
+            <ProductsToAdd/>
+            <TotalAmount/>
             <button>Validar Nota</button>
         </ValidateResponseStyle>
     )
