2084df2eefcf600e4d57da8cc1ca1b8d6b05c9f1
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-10-25 17:25:18 -0300

    use context to clean the code

--- src/pages/AddEstoque/index.jsx
+++ src/pages/AddEstoque/index.jsx
@@ -1,46 +1,17 @@
 import { AddEstoqueContainerStyle, AddEstoqueContentStyle } from "./style";
-import getXMLConverter from "../../api/xmlConverter";
-import { useEffect, useState } from "react";
 import IntroForm from "./IntroForm";
 import ValidateResponse from "./ValidateResponse";
+import {useAddEstoqueContext} from "../../contexts/AddEstoque"
 
 export default function AddEstoque() {
-  const [notaFiscal, setNotaFiscal] = useState(null);
-  const [isLoadingNotafiscal, setIsLoadingNotaFiscal] = useState(false);
 
-  const handleChangeNotaFiscal = (e) => {
-    setIsLoadingNotaFiscal(true);
-    setNotaFiscal(e.target.files[0]);
-  };
-
-  useEffect(() => {
-    if (isLoadingNotafiscal) {
-      setTimeout(async () => {
-        const convertResult = await getXMLConverter(notaFiscal);
-        setIsLoadingNotaFiscal(false);
-        console.log(convertResult);
-      }, 1000);
-    }
-  }, [isLoadingNotafiscal]);
-
-  const handleSubmitNotaFiscal = async (e) => {
-    e.preventDefault();
-  };
+  const { isLoadNFe } = useAddEstoqueContext();
 
   return (
     <AddEstoqueContainerStyle>
-      <AddEstoqueContentStyle isLoad={isLoadingNotafiscal}>
-        {false ? (
-          <IntroForm
-            handleSubmitNotaFiscal={handleSubmitNotaFiscal}
-            handleChangeNotaFiscal={handleChangeNotaFiscal}
-            notaFiscal={notaFiscal}
-            isLoadingNotafiscal={isLoadingNotafiscal}
-          />
-        ) : (
-          <ValidateResponse />
-        )}
-      </AddEstoqueContentStyle>
+        <AddEstoqueContentStyle isLoad={isLoadNFe}>
+          {isLoadNFe ? <ValidateResponse /> : <IntroForm />}
+        </AddEstoqueContentStyle>
     </AddEstoqueContainerStyle>
   );
 }
