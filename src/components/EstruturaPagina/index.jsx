0fdb3140dfbd2d2f610c25344f2d9ec989135287
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-10-25 17:19:22 -0300

    remove "Header" component

--- src/components/EstruturaPagina/index.jsx
+++ src/components/EstruturaPagina/index.jsx
@@ -2,14 +2,12 @@ import { useState } from "react";
 // Import Style
 import { EstruturaPaginaStyle, ConteudoPaginaStyle } from "./style";
 // Import Components
-import Header from "./Header";
 import NavBar from "./NavBar";
 
-import { useThemeContext } from "../../contexts/ThemeContext";
+import { useThemeContext } from "../../contexts/Theme";
 
 export default function EstruturaPagina({ children }) {
   const [navBarIsActive, setNavBarIsActive] = useState(false);
-
   const { theme } = useThemeContext();
 
   return (
