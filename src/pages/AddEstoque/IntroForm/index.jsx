67c0d46e77bfe1e051597ee3f730a7dba8091e32
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-10-25 17:27:06 -0300

    clean the code and rename "isLoadingNotafiscal" to  "isLoadingNFe"

--- src/pages/AddEstoque/IntroForm/index.jsx
+++ src/pages/AddEstoque/IntroForm/index.jsx
@@ -1,33 +1,36 @@
 import { IntroFormStyle } from "./style";
 import Loading from "../../../components/Loading";
+import { useAddEstoqueContext } from "../../../contexts/AddEstoque";
 
-export default function IntroForm({
+export default function IntroForm() {
+  const {
+    notaFiscal,
+    isLoadingNFe,
     handleSubmitNotaFiscal,
     handleChangeNotaFiscal,
-    notaFiscal,
-    isLoadingNotafiscal
-}) {
+  } = useAddEstoqueContext();
+
   return (
-      <IntroFormStyle>
-        <h2>Adicione um novo produto no Estoque</h2>
-        <form onSubmit={handleSubmitNotaFiscal}>
-          <div>
-            <label htmlFor="input-convert-xml-to-notafiscal">
-              <span>Escolha um Arquivo</span>
-            </label>
-            <span>
-              {notaFiscal ? notaFiscal.name : "Nenhum arquivo selecionado"}
-            </span>
-            <input
-              id="input-convert-xml-to-notafiscal"
-              type="file"
-              accept="application/xml"
-              onChange={handleChangeNotaFiscal}
-            />
-          </div>
-          <Loading isLoading={isLoadingNotafiscal} />
-          <button type="submit">Abrir Arquivo</button>
-        </form>
-      </IntroFormStyle>
+    <IntroFormStyle>
+      <h2>Adicione um novo produto no Estoque</h2>
+      <form onSubmit={handleSubmitNotaFiscal}>
+        <div>
+          <label htmlFor="input-convert-xml-to-notafiscal">
+            <span>Escolha um Arquivo</span>
+          </label>
+          <span>
+            {notaFiscal ? notaFiscal.name : "Nenhum arquivo selecionado"}
+          </span>
+          <input
+            id="input-convert-xml-to-notafiscal"
+            type="file"
+            accept="application/xml"
+            onChange={handleChangeNotaFiscal}
+          />
+        </div>
+        <Loading isLoading={isLoadingNFe} />
+        <button type="submit">Abrir Arquivo</button>
+      </form>
+    </IntroFormStyle>
   );
 }
