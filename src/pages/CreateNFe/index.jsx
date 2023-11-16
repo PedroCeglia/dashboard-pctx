9279206cb26fba8e9fd2b565aba93ff4f7592795
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-11-16 18:26:59 -0300

    Create CreateNFe Component

---
+++ src/pages/CreateNFe/index.jsx
@@ -0,0 +1,10 @@
+import FormNFe from "./FormNFe"
+import { CreateNFeStyle } from "./style"
+export default function CreateNFe(){
+    return(
+        <CreateNFeStyle>
+            <h1>Criando uma NFe</h1>
+            <FormNFe/>
+        </CreateNFeStyle>
+    )
+}
---
+++ src/pages/CreateNFe/style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const CreateNFeStyle = styled.div`
+    width: 100%;
+    min-height: 100vh;
+    padding: 20px;
+    background-color: #efefef;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+
+`
